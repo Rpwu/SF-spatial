be8bc6895f1d27185174552b6fc3602baccc3b19
Rpwu <ryanwu2000rpw@gmail.com>
2024-06-03 21:48:27 -0400

    intial commit

---
+++ classes/mapping.py
@@ -0,0 +1,247 @@
+neighborhood_mapping = {
+    # Bayview Hunters Point
+    'Little Hollywood': 'Bayview Hunters Point',
+
+    'Bayview': 'Bayview Hunters Point',
+
+    'Produce Market': 'Bayview Hunters Point',
+
+    'Hunters Point': 'Bayview Hunters Point',
+
+    'Silver Terrace': 'Bayview Hunters Point',
+
+    'Apparel City': 'Bayview Hunters Point',
+
+    'Candlestick Point SRA': 'Bayview Hunters Point',
+
+    'Bret Harte': 'Bayview Hunters Point',
+
+    'India Basin': 'Bayview Hunters Point', 
+
+    # Bernal Heights
+    'Bernal Heights': 'Bernal Heights',
+
+    'Peralta Heights': 'Bernal Heights',
+
+    'Holly Park': 'Bernal Heights',  
+
+    # Castro/Upper Market
+    'Corona Heights': 'Castro/Upper Market',
+    'Eureka Valley': 'Castro/Upper Market',
+    'Castro': 'Castro/Upper Market',
+    'Clarendon Heights': 'Castro/Upper Market',
+
+    # Chinatown
+    'Chinatown': 'Chinatown',
+
+    # Excelsior
+    'Excelsior': 'Excelsior',
+
+    'University Mound': 'Excelsior',
+
+    'Crocker Amazon': 'Excelsior',
+
+    # Financial District/South Beach
+    'Financial District': 'Financial District/South Beach',
+
+    'Downtown / Union Square': 'Financial District/South Beach',
+
+    'Rincon Hill': 'Financial District/South Beach',
+
+    'South Beach': 'Financial District/South Beach',
+
+    'Northern Waterfront': 'Financial District/South Beach',
+
+    'Central Waterfront': 'Financial District/South Beach',
+
+    # Glen Park
+    'Fairmount': 'Glen Park',
+
+    'Glen Park': 'Glen Park',
+
+    # Golden Gate Park
+    'Golden Gate Park': 'Golden Gate Park',
+
+    'Panhandle': 'Golden Gate Park',
+
+    # Haight Ashbury
+    'Buena Vista': 'Haight Ashbury',
+
+    'Ashbury Heights': 'Haight Ashbury',
+
+    'Cole Valley': 'Haight Ashbury',
+
+    'Haight Ashbury': 'Haight Ashbury',
+
+    # Hayes Valley
+    'Lower Haight': 'Hayes Valley',
+
+    'Mint Hill': 'Hayes Valley',
+
+    'Alamo Square': 'Hayes Valley',
+
+    'Hayes Valley': 'Hayes Valley',
+
+    'Duboce Triangle': 'Hayes Valley',
+
+    # Inner Richmond
+    'Inner Richmond': 'Inner Richmond',
+
+    'Presidio Terrace': 'Inner Richmond',
+
+    # Inner Sunset
+    'Forest Knolls': 'Inner Sunset',
+
+    'Inner Sunset': 'Inner Sunset',
+
+    'Parnassus Heights': 'Inner Sunset',
+
+    'Golden Gate Heights': 'Inner Sunset',
+
+    # Japantown
+    'Japantown': 'Japantown',
+
+    # Lakeshore
+    'Lakeshore': 'Lakeshore',
+
+    'Parkmerced': 'Lakeshore',
+
+    # Lincoln Park
+    'Lincoln Park / Ft. Miley': 'Lincoln Park',
+
+    # Lone Mountain/USF
+    'Anza Vista': 'Lone Mountain/USF',
+
+    'Lone Mountain': 'Lone Mountain/USF',
+
+    # Marina
+    'Cow Hollow': 'Marina',
+
+    'Union Street': 'Marina',
+
+    'Aquatic Park / Ft. Mason': 'Marina',
+
+    'Marina': 'Marina',
+
+    # McLaren Park
+    'McLaren Park': 'McLaren Park',
+
+    # Mission
+    'Mission': 'Mission',
+
+    'Dolores Heights': 'Mission',
+
+    'Mission Dolores': 'Mission',
+
+    # Mission Bay
+    'Mission Bay': 'Mission Bay',
+
+    # Nob Hill
+    'Nob Hill': 'Nob Hill',
+
+    # Noe Valley
+    'Diamond Heights': 'Noe Valley',
+
+    'Noe Valley': 'Noe Valley',
+
+    'Upper Market': 'Noe Valley',
+
+    # North Beach
+    "Fisherman's Wharf": 'North Beach',  
+
+    'North Beach': 'North Beach',
+
+    'Telegraph Hill': 'North Beach',
+
+    # Oceanview/Merced/Ingleside
+    'Oceanview': 'Oceanview/Merced/Ingleside',
+
+    'Merced Heights': 'Oceanview/Merced/Ingleside',
+
+    'Ingleside': 'Oceanview/Merced/Ingleside',
+
+    # Outer Mission
+    'Outer Mission': 'Outer Mission',
+
+    'Cayuga': 'Outer Mission',
+
+    "St. Mary's Park": 'Outer Mission',  
+
+    'Mission Terrace': 'Outer Mission',
+
+    # Outer Richmond
+    'Lake Street': 'Outer Richmond',
+
+    'Sutro Heights': 'Outer Richmond',
+
+    'Outer Richmond': 'Outer Richmond',
+
+    # Pacific Heights
+    'Pacific Heights': 'Pacific Heights',
+
+    # Portola
+    'Portola': 'Portola',
+
+    # Portrero Hill
+    'Potrero Hill': 'Potrero Hill',
+
+    # Presidio Heights
+    'Presidio Heights': 'Presidio Heights',
+
+    'Lower Pacific Heights': 'Presidio Heights',
+
+    'Laurel Heights / Jordan Park': 'Presidio Heights',
+
+    # Russian Hill
+    'Russian Hill': 'Russian Hill',
+
+    # Seacliff
+    'Seacliff': 'Seacliff',
+
+    # South of Market
+    'South of Market': 'South of Market',
+
+    'Showplace Square': 'South of Market',
+
+    # Sunset/Parkside
+    'Parkside': 'Sunset/Parkside',
+
+    'Outer Sunset': 'Sunset/Parkside',
+
+    'Merced Manor': 'Sunset/Parkside',
+
+    # Tenderlion
+    'Tenderloin': 'Tenderloin',
+
+    'Civic Center': 'Tenderloin',
+
+    # Treasure Island
+    'Treasure Island': 'Treasure Island',
+
+    # Visitacion Valley
+    'Visitacion Valley': 'Visitacion Valley',
+
+    # West of Twin Peaks
+    'Stonestown': 'West of Twin Peaks',
+
+    'Laguna Honda': 'West of Twin Peaks',
+
+    'Sunnyside': 'West of Twin Peaks',
+
+    'Westwood Park': 'West of Twin Peaks',
+
+    'Forest Hill': 'West of Twin Peaks',
+
+    'Mt. Davidson Manor': 'West of Twin Peaks',
+
+    'Miraloma Park': 'West of Twin Peaks',
+
+    'Ingleside Terraces': 'West of Twin Peaks',
+
+    'West Portal': 'West of Twin Peaks',
+
+    # Western Addition
+    'Cathedral Hill': 'Western Addition',
+
+    'Western Addition': 'Western Addition'
+}
